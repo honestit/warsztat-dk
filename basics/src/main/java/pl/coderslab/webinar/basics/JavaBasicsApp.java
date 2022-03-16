a0fd96000d157d64f8a5632c3fa1778a67865752
honestit <michal.kupisinski@gmail.com>
2022-03-16 11:07:06 +0100

    Remove base app content

--- basics/src/main/java/pl/coderslab/webinar/basics/JavaBasicsApp.java
+++ basics/src/main/java/pl/coderslab/webinar/basics/JavaBasicsApp.java
@@ -4,14 +4,10 @@ package pl.coderslab.webinar.basics;
  *  Tytuł (link)
  *     Opis
  *
- *  Wojna jako siła wyższa w zamówieniach publicznych (https://rp.pl/abc-firmy/art35837231-wojna-jako-sila-wyzsza-w-zamowieniach-publicznych)
- *     Od dwóch lat wykonawcy zamówień publicznych są zmuszeni odwoływać się do instytucji siły wyższej, by uchronić się od odpowiedzialności za opóźnienia w realizacji kontraktów. Myśleliśmy, że o sile wyższej napisano już wszystko. Tymczasem wojna w Ukrainie wymusza powołanie się na siłę wyższą w innym kontekście.
  */
 public class JavaBasicsApp {
 
     public static void main(String[] args) {
-        System.out.println("Wojna jako siła wyższa w zamówieniach publicznych (https://rp.pl/abc-firmy/art35837231-wojna-jako-sila-wyzsza-w-zamowieniach-publicznych)");
-        System.out.println("    Od dwóch lat wykonawcy zamówień publicznych są zmuszeni odwoływać się do instytucji siły wyższej, by uchronić się od odpowiedzialności za opóźnienia w realizacji kontraktów. Myśleliśmy, że o sile wyższej napisano już wszystko. Tymczasem wojna w Ukrainie wymusza powołanie się na siłę wyższą w innym kontekście.");
     }
 
 
