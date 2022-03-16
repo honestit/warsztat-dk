3df98b2cf2a5ef468fbfe038f86d58f48f1d2974
honestit <michal.kupisinski@gmail.com>
2022-03-16 10:58:17 +0100

    Initial commit

---
+++ console/src/main/java/pl/coderslab/webinar/console/ConsoleArticlesApp.java
@@ -0,0 +1,9 @@
+package pl.coderslab.webinar.console;
+
+import java.util.List;
+
+public class ConsoleArticlesApp {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ web/src/main/java/pl/coderslab/webinar/web/ArticlesWebApp.java
@@ -0,0 +1,12 @@
+package pl.coderslab.webinar.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesWebApp.class, args);
+    }
+}
